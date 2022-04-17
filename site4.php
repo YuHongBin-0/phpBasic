e92f5fa41285d6e70b78dd066037056cdb5b12a3
YuHongBin-0 <yuhongbin124@gmail.com>
2022-04-17 19:44:33 +0800

    finish the tutorial - basic php language

--- site4.php
+++ site4.php
@@ -116,9 +116,47 @@
     echo $avengers -> getRating();
 
 
-
-
 ?>
+
+<!-- Inheritance below -->
+<hr>
+<br>
+<?php 
+
+class Chef {
+    function makeChicken(){
+        echo "The chef makes chicken <br>";
+    }
+
+    function makeSalad(){
+        echo "The chef makes salad <br>";
+    }
+
+    function makeSpecialDish(){
+        echo "The chef makes bbq ribs <br>";
+    }
+
+}
+
+class ItalianChef extends Chef{
+    function makePasta(){
+        echo "the chef makes pasta";
+    }
+
+    function makeSpecialDish(){
+        echo "the chef make chicken parm";
+    }
+}
+
+$chef = new Chef();
+$chef -> makeChicken();
+
+$italianChef = new ItalianChef();
+$italianChef -> makeSpecialDish();
+    
+?>
+
+
 
 
     </body>
